f030392c91ce825bd4b8e1775bea9a54dbbf6634
gamxu <wuttipat.did@gmail.com>
2022-01-14 16:36:30 +0700

    lab16_4 completed

--- lab16_4.cpp
+++ lab16_4.cpp
@@ -17,3 +17,25 @@ int main(){
 	
 	return 0;
 }
+
+void swap (int *x, int *y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+ 
+
+void shuffle(int &A,int &B,int &C,int &D){
+	
+	int cs = rand()%6;
+	if(cs==0) swap(A,B);
+	else if(cs==1) swap(A,C);
+	else if(cs==2) swap(A,D);
+	else if(cs==3) swap(B,C);
+	else if(cs==4) swap(B,D);
+	else swap(C,D);
+
+	return;
+}
+
